7e3ac21b0e6421257b39ba3ad340f4ea958f62e4
Santiago Pinilla <johansantiagopinillauribe@gmail.com>
2020-03-01 00:41:38 -0500

    cat

--- app/src/main/java/com/example/kotlinproyect/MainActivity.kt
+++ app/src/main/java/com/example/kotlinproyect/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.kotlinproyect
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_cat.setOnClickListener {
+            var song: MediaPlayer? = MediaPlayer.create(this, R.raw.friends)
+            song?.start()
+        }
     }
 }
